fdddbd68d5554247f2b383abeae762ef3206d559
Ivan A. W. <45644511+zardvans@users.noreply.github.com>
2026-03-31 17:20:54 +0800

    feat(03-11): Route Group

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HelloWorldController::class, 'index']);
 
 Route::get('/members/{id}', [MembersController::class, 'show'])->whereNumber('id')->name('members.show');
+
+Route::group([],function () {
+    Route::get('hello', function () {
+        return "Hello";
+    });
+
+    Route::get('world', function () {
+        return "World";
+    });
+});
