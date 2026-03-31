c26f28103f04407cceb7fb0032ebdf819cc3f83a
Ivan A. W. <45644511+zardvans@users.noreply.github.com>
2026-03-31 17:57:04 +0800

    init(laravel): Install Laravel Breeze for Simple Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
-use App\Http\Controllers\HelloWorldController;
-use App\Http\Controllers\MembersController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HelloWorldController::class, 'index']);
-
-Route::get('/members/{id}', [MembersController::class, 'show'])->whereNumber('id')->name('members.show');
-
-Route::group([],function () {
-    Route::get('hello', function () {
-        return "Hello";
-    });
-
-    Route::get('world', function () {
-        return "World";
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
