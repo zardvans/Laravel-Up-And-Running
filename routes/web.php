50b411074657d90b4183ef46b4f19df5c94c3207
Ivan A. W. <45644511+zardvans@users.noreply.github.com>
2026-03-31 15:55:49 +0800

    feat(03-03): Route Verbs

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('hello-world');
 });
+
+Route::post('/', function() {
+    return 'This is a post route';
+});
+
+
+Route::put('/', function() {
+    return 'This is a put route';
+});
+
+
+Route::delete('/', function() {
+    return 'This is a delete route';
+});
+
+Route::any('/', function() {
+    return 'This is any route';
+});
+
+Route::match(['get', 'post'], '/', function() {
+    return 'This is a match route';
+});
