e88904ca66344c76fde331772617a699ded41d7a
Ivan A. W. <45644511+zardvans@users.noreply.github.com>
2026-04-01 15:56:59 +0800

    feat(03-13): Path Prefixes

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', function () {
+        return 'Path is /dashboard';
+    });
+    Route::get('users', function () {
+        return 'Path is /dashboard/users';
+    });
 });
 
-require __DIR__.'/auth.php';
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
