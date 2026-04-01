0f7b71bd1b569d14718b6ce9f30c23a78324063e
Ivan A. W. <45644511+zardvans@users.noreply.github.com>
2026-04-01 17:45:52 +0800

    feat(03-17.5): Fallback and Signed Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -46,6 +47,12 @@ Route::controller(UserController::class)->group(function () {
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
 });
+
+Route::fallback(function () {
+    return 'This is a fallback route.';
+});
+
+Route::get('invitations/{invitation}/{answer}', InvitationController::class)->name('invitations')->middleware('signed');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
