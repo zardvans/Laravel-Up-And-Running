16893ef283a63a9934cf02a3bfb1103844b2a2dc
Ivan A. W. <45644511+zardvans@users.noreply.github.com>
2026-04-04 16:38:25 +0800

    feat(03-36): CSRF and form method spoofing

--- resources/views/hello-world.blade.php
+++ resources/views/hello-world.blade.php
@@ -2,4 +2,8 @@
     Hello World from Route Handling!
     <br/>
     Example link with url helper: <a href="{{ url('/') }}">Home</a>
+    <form action='tasks/5' method='POST'>
+        @csrf
+        @method('DELETE')
+        <button type='submit'>Delete Task 5</button>
 </div>
